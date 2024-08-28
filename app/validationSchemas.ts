ed2dfe842965d13fc74b4a28bb12cad81d4b05ff
cait <moffitt.cait@gmail.com>
2024-08-28 17:54:26 -0400

    Build an API for creating tasks

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 
-export const createIssueSchema = z.object({
+export const createTaskSchema = z.object({
     title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required.')
+    description: z.string().min(1, 'Description is required.'),
+    dueDate: z.coerce.date() // Converts input string into date type
 });
