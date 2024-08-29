7041e45c05fa588a4e0fe014a28777881ce167ae
cait <moffitt.cait@gmail.com>
2024-08-28 23:17:20 -0400

    Remove Text import

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import * as Label from "@radix-ui/react-label";
 import { useForm, Controller } from "react-hook-form";
