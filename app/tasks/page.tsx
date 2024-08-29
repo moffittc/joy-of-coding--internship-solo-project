d33c7177686908d0a52c99077d189e2cc246806f
cait <moffitt.cait@gmail.com>
2024-08-29 16:42:06 -0400

    Retrieve all the tasks from the database

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Checkbox, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const TasksPage = () => {
+const TasksPage = async () => {
+  // Get all the tasks from the server
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      dueDate: "asc",
+    },
+    select: {
+      completed: true,
+      title: true,
+      description: true,
+      dueDate: true,
+    },
+  });
+  console.log("allTasks: ", allTasks);
   return (
     <div>
       <Button>
