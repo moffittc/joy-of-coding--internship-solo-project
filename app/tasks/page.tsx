a0411a65e4776e6dc6d9e71edcbb7254d064d309
cait <moffitt.cait@gmail.com>
2024-08-30 00:45:59 -0400

    Display the tasks from the database

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,27 +1,43 @@
 import React from "react";
-import { Button, Checkbox, Table } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
+import DoneCheckbox from "@/app/components/DoneCheckbox";
 import Link from "next/link";
 import prisma from "@/prisma/client";
 
 const TasksPage = async () => {
   // Get all the tasks from the server
   const allTasks = await prisma.task.findMany({
+    // In order by due date
     orderBy: {
       dueDate: "asc",
     },
     select: {
+      id: true,
       completed: true,
       title: true,
       description: true,
       dueDate: true,
     },
   });
-  console.log("allTasks: ", allTasks);
+  // Executed when page is refreshed:
+  console.log("allTasks: ", allTasks[0].completed);
+  // How to display them in a table?
+
   return (
     <div>
       <Button>
         <Link href="/tasks/new">+</Link>
       </Button>
+      {allTasks.map((task) => (
+        <p key={task.id}>
+          <DoneCheckbox />
+          {task.title +
+            " | " +
+            task.description +
+            " | " +
+            task.dueDate.toDateString()}
+        </p>
+      ))}
     </div>
   );
 };
