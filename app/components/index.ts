1c480a8b30d48fce02efba3f08ce9d40988a99b6
cait <moffitt.cait@gmail.com>
2024-10-18 03:40:27 -0400

    Build the edit task page

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,4 +2,4 @@ export { default as DeleteButton } from "./DeleteButton";
 export { default as DoneCheckbox } from "./DoneCheckbox";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as Spinner } from "./Spinner";
-export { default as TaskCategoryBadge } from "./taskCategoryBadge";
+export { default as TaskCategoryBadge } from "./TaskCategoryBadge";
---
+++ app/tasks/_components/TaskForm.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import { DeleteButton, ErrorMessage, Spinner } from "@/app/components";
+import { Category, formSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Task } from "@prisma/client";
+import * as Label from "@radix-ui/react-label";
+import {
+  Button,
+  Callout,
+  Heading,
+  Select,
+  TextArea,
+  TextField,
+} from "@radix-ui/themes";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type TaskFormData = z.infer<typeof formSchema>;
+
+const TaskForm = ({ task }: { task?: Task }) => {
+  const router = useRouter();
+  // const searchParams = useSearchParams(); // For retrieving task data
+  // const taskID = searchParams.get("id");
+  // const taskTitle = searchParams.get("title");
+  // const taskDesc = searchParams.get("description");
+  // const taskCategory = searchParams.get("category");
+  // const taskYear = searchParams.get("ddYear");
+  // const taskMonth = searchParams.get("ddMonth");
+  // const taskDay = searchParams.get("ddDay");
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<TaskFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      // Sends data to api:
+      if (data.type === "post") {
+        await axios.post("/api/tasks", data.data);
+      } else if (data.type === "patch") {
+        await axios.patch("/api/tasks", data.data);
+      }
+      router.push("/tasks");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred.");
+    }
+  });
+
+  return (
+    <div className="max-w-xl">
+      <Heading>{task ? "Edit Task" : "Create Task"}</Heading>
+
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={onSubmit}>
+        {/* Determines whether to use the post or patch schema */}
+        <input
+          type="hidden"
+          value={task ? "patch" : "post"}
+          {...register("type")}
+        />
+
+        {/* ID Field - for editing a task */}
+        {task && (
+          <input type="hidden" value={task?.id} {...register("data.id")} />
+        )}
+
+        {/* Title Field*/}
+        <TextField.Root
+          placeholder="Title"
+          defaultValue={task?.title}
+          {...register("data.title")}
+        />
+        <ErrorMessage>{errors.data?.title?.message}</ErrorMessage>
+
+        {/* Description Field */}
+        <TextArea
+          placeholder="Description"
+          defaultValue={task?.description}
+          {...register("data.description")}
+        />
+        <ErrorMessage>{errors.data?.description?.message}</ErrorMessage>
+
+        <div className="flex justify-between">
+          {/* Category Field */}
+          <div className="flex flex-col w-1/2 mr-10">
+            <Label.Root
+              className="text-[15px] font-medium leading-[35px]"
+              htmlFor="category"
+            >
+              Priority:
+            </Label.Root>
+            <Select.Root
+              defaultValue={task?.category}
+              onValueChange={(value) =>
+                setValue("data.category", value as Category)
+              }
+            >
+              <Select.Trigger placeholder="Select a category" />
+              <Select.Content id="category">
+                <Select.Item value={Category.None}>None</Select.Item>
+                <Select.Item value={Category.High}>High</Select.Item>
+                <Select.Item value={Category.Medium}>Medium</Select.Item>
+                <Select.Item value={Category.Low}>Low</Select.Item>
+              </Select.Content>
+            </Select.Root>
+            <ErrorMessage>{errors.data?.category?.message}</ErrorMessage>
+          </div>
+
+          {/* Due Date Field - returns a string */}
+          <div className="flex flex-col w-1/2">
+            <Label.Root
+              className="text-[15px] font-medium leading-[35px]"
+              htmlFor="dueDate"
+            >
+              Due Date:
+            </Label.Root>
+            <input
+              type="date"
+              id="dueDate"
+              defaultValue={
+                task?.dueDate.toDateString()
+                // taskDay ? taskYear + "-" + taskMonth + "-" + taskDay : undefined
+              }
+              {...register("data.dueDate")}
+            />
+            <ErrorMessage>{errors.data?.dueDate?.message}</ErrorMessage>
+          </div>
+        </div>
+
+        {/*Submit Button*/}
+        <Button disabled={isSubmitting}>
+          {task ? "Update" : "Create"}
+          {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+
+      {/*Cancel Button*/}
+      <Button>
+        <Link href="/tasks">Cancel</Link>
+      </Button>
+
+      {/*Delete Button*/}
+      {task && <DeleteButton id={task.id} href="/tasks" />}
+    </div>
+  );
+};
+
+export default TaskForm;
