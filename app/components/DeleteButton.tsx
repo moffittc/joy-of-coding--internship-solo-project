896cfff210ecfe8a82661af8a65ab2c4ba5ef8d5
cait <moffitt.cait@gmail.com>
2024-10-21 18:35:40 -0400

    Update delete button to use an Alert Dialog

--- app/components/DeleteButton.tsx
+++ app/components/DeleteButton.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { CancelButton, Spinner } from "@/app/components";
-import { AlertDialog, Button, Callout, Flex } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 interface Props {
   id: number;
@@ -12,39 +10,14 @@ interface Props {
 }
 
 const DeleteButton = ({ id, href }: Props) => {
-  // const router = useRouter();
-  // const [error, setError] = useState("");
-  // const [isDeleting, setDeleting] = useState(false);
-
-  // const handleDelete = async () => {
-  //   try {
-  //     setDeleting(true);
-  //     await axios.delete(`/api/tasks/${id}`);
-  //     router.push(`${href}`);
-  //     router.refresh();
-  //   } catch (error) {
-  //     setDeleting(false);
-  //     setError("An unexpected error occurred.");
-  //   }
-  // };
+  const router = useRouter();
 
   return (
     <div>
-      {/* {error && (
-        <Callout.Root color="red" className="mb-5">
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )} */}
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button
-            // disabled={isDeleting}
-            // onClick={handleDelete}
-            color="tomato"
-            variant="soft"
-          >
+          <Button color="tomato" variant="soft">
             Delete
-            {/* {isDeleting && <Spinner />} */}
           </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
@@ -60,7 +33,16 @@ const DeleteButton = ({ id, href }: Props) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button color="red">Delete</Button>
+              <Button
+                color="red"
+                onClick={async () => {
+                  await axios.delete("/api/tasks/" + id);
+                  router.push(`${href}`);
+                  router.refresh();
+                }}
+              >
+                Delete
+              </Button>
             </AlertDialog.Action>
           </Flex>
         </AlertDialog.Content>
