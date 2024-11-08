1bb7d796090247e5caa2430e89425406f1cd4c1f
cait <moffitt.cait@gmail.com>
2024-11-08 03:59:25 -0500

    Remove old code

--- app/tasks/EditButton.tsx
+++ app/tasks/EditButton.tsx
@@ -1,30 +1,9 @@
 import Link from "next/link";
 import { TbPencil } from "react-icons/tb";
 
-// type Task = {
-//   dueDate: Date;
-//   id: number;
-//   title: string;
-//   description: string;
-//   completed: boolean;
-//   category: Category;
-// };
-
 const EditButton = ({ taskID }: { taskID: number }) => {
   return (
-    <Link
-      href={`/tasks/${taskID}`}
-      // href={`/tasks/new?id=${task.id}&title=${task.title}&description=${
-      //   task.description
-      // }&category=${task.category}&ddYear=${task.dueDate.toLocaleDateString(
-      //   "en-US",
-      //   { year: "numeric" }
-      // )}&ddMonth=${task.dueDate.toLocaleDateString("en-US", {
-      //   month: "2-digit",
-      // })}&ddDay=${task.dueDate.toLocaleDateString("en-US", {
-      //   day: "2-digit",
-      // })}`}
-    >
+    <Link href={`/tasks/${taskID}`}>
       <TbPencil />
     </Link>
   );
